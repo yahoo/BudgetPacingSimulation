fa23a43193698b80618f0cbaf0ac4ae3689ea398
rstram <rotem.stram@oath.com>
2023-07-17 15:10:27 +0300

    Updating the addCampaign unit test

--- tests/mystique_tests/mystique_campaign_initialization.py
+++ tests/mystique_tests/mystique_campaign_initialization.py
@@ -1,5 +1,6 @@
 from src.budget_pacing.mystique.mystique_tracked_campaign import MystiqueTrackedCampaign
 from src.campaign import Campaign
+import src.budget_pacing.mystique.mystique_constants as mystique_constants
 
 
 def instance_for_target_slope_test():
@@ -8,3 +9,7 @@ def instance_for_target_slope_test():
 
 def instance_for_mystique_test_init():
     return Campaign(0, 100, 10, 0.5, [])
+
+
+def instance_for_budget_above_threshold():
+    return Campaign(1, mystique_constants.min_daily_budget_for_high_initialization + 1, 1, 0.5, [])
--- tests/mystique_tests/mystique_pacing_system_test.py
+++ tests/mystique_tests/mystique_pacing_system_test.py
@@ -4,6 +4,7 @@ from src.budget_pacing.mystique.target_slope import TargetSpendStrategyType
 from src.budget_pacing.mystique.mystique import MystiquePacingSystem
 from src.budget_pacing.mystique.mystique_tracked_campaign import MystiqueTrackedCampaign
 import mystique_campaign_initialization
+import src.budget_pacing.mystique.mystique_constants as mystique_constants
 
 
 class TestMystiquePacingSystem(unittest.TestCase):
@@ -19,6 +20,22 @@ class TestMystiquePacingSystem(unittest.TestCase):
         self.assertTrue(campaign_id in self.mystique_linear.mystique_tracked_campaigns.keys(), "Campaign not added to Mystique's tracked campaigns")
 
         mystique_tracked_campaign = self.mystique_linear.mystique_tracked_campaigns[campaign_id]
-        self.assertEqual(campaign.daily_budget, mystique_tracked_campaign.daily_budget, "daily budget initialization not correct")
+        self.assertEqual(mystique_tracked_campaign.daily_budget, campaign.daily_budget, "daily budget initialization not correct")
+        self.assertEqual(mystique_tracked_campaign.ps, mystique_constants.pacing_signal_for_initialization, "pacing signal initialization not correct")
+        self.assertEqual(mystique_tracked_campaign.previous_ps, mystique_constants.pacing_signal_for_initialization, "previous pacing signal initialization not correct")
+        self.assertEqual(mystique_tracked_campaign.last_positive_ps, mystique_constants.pacing_signal_for_initialization, "last positive pacing signal initialization not correct")
+
+        campaign = mystique_campaign_initialization.instance_for_budget_above_threshold()
+        self.mystique_linear.add_campaign(campaign)
+
+        campaign_id = campaign.campaign_id
+        self.assertTrue(campaign_id in self.mystique_linear.mystique_tracked_campaigns.keys(), "Campaign not added to Mystique's tracked campaigns")
+
+        mystique_tracked_campaign = self.mystique_linear.mystique_tracked_campaigns[campaign_id]
+        self.assertEqual(mystique_tracked_campaign.daily_budget, campaign.total_budget, "daily budget initialization not correct")
+        self.assertEqual(mystique_tracked_campaign.ps, mystique_constants.max_ps, "pacing signal initialization not correct")
+        self.assertEqual(mystique_tracked_campaign.previous_ps, mystique_constants.max_ps, "previous pacing signal initialization not correct")
+        self.assertEqual(mystique_tracked_campaign.last_positive_ps, mystique_constants.max_ps, "last positive pacing signal initialization not correct")
+
 
 
