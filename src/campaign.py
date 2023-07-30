2bd666000a3da6f212a335873531a6415406045e
rabboud <rani.abboud@yahooinc.com>
2023-07-30 14:02:53 +0300

    minimal bid is defined as a constant

--- src/campaign.py
+++ src/campaign.py
@@ -1,25 +1,26 @@
 import random
 from typing import Optional
 
-from src.auction import AuctionInterface
+from src import constants
 from src.bid import Bid
 
 
 class Campaign:
     def __init__(self, campaign_id: str, total_budget: float, run_period: float, max_bid: float, targeting_group):
         self.id = campaign_id
+        if max_bid <= constants.MINIMAL_BID:
+            raise Exception('Invalid max_bid parameter.')
+        self.max_bid = max_bid
         self.total_budget = total_budget
         self.run_period = run_period
         self.max_bid = max_bid
         self.targeting_group = targeting_group
         self.daily_budget = total_budget / run_period
 
-    def bid(self, auction: AuctionInterface) -> Optional[Bid]:
-        if self.max_bid < auction.min_bid():
-            return None
-        bid_amount = random.uniform(auction.min_bid(), self.max_bid)
+    def bid(self) -> Optional[Bid]:
+        bid_amount = random.uniform(constants.MINIMAL_BID, self.max_bid)
         # Only in Step 4 we start tracking the campaigns' budgets
         # if bid_amount > self.daily_budget:
         #     bid_amount = self.daily_budget
-
         return Bid(campaign_id=self.id, amount=bid_amount)
+
--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,2 @@
 AUCTIONS_PER_MINUTE = 1000  # temporarily defining this value, will later sample from a distribution instead
-AUCTIONS_MIN_BID_DEFAULT = 0.01
+MINIMAL_BID = 0.01
