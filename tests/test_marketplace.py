dbb95a8ca8695300160212aefa46a5d265b20f56
rabboud <rani.abboud@yahooinc.com>
2023-08-21 15:45:13 +0300

    sampling the number of auctions for each minute from a distribution

--- tests/test_marketplace.py
+++ tests/test_marketplace.py
@@ -10,7 +10,6 @@ class TestMarketPlace(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         Clock.reset()
-        config.num_auctions_per_iteration = 10
         config.num_spend_entries_per_day = 24
         config.num_win_entries_per_day = 24 * 60
         config.num_untracked_bids = 0
@@ -18,12 +17,15 @@ class TestMarketPlace(unittest.TestCase):
     def test_marketplace(self):
         num_days = 3
         num_campaigns = 10
+        num_auctions_per_iteration = 10
         campaigns = []
         for i in range(num_campaigns):
             campaigns.append(
                 Campaign(campaign_id=f'campaign_{i}', total_budget=1000000, run_period=7, max_bid=25)
             )
         serving_system = ServingSystem(tracked_campaigns=campaigns)
+        # replacing the function that calculates the number of auctions for each minute
+        Marketplace._calculate_number_of_current_auctions = lambda _: num_auctions_per_iteration
         marketplace = Marketplace(serving_system=serving_system)
         for day in range(num_days):
             for i in range(config.num_iterations_per_day):
@@ -33,7 +35,7 @@ class TestMarketPlace(unittest.TestCase):
                 [sum(c.stats.auctions_won_history[-1]) for c in campaigns]
             )
             self.assertEqual(num_auctions_won_last_day,
-                             config.num_auctions_per_iteration * config.num_iterations_per_day,
+                             num_auctions_per_iteration * config.num_iterations_per_day,
                              "expected the total number of wins in a day to be equal to the number of auctions")
 
 
