c6fdb6bcfaf08b35b81303713e99f1b11d68c79f
rabboud <rani.abboud@yahooinc.com>
2023-08-24 16:13:41 +0300

    unit tests for traffic distribution mean calculation

--- tests/test_marketplace.py
+++ tests/test_marketplace.py
@@ -7,8 +7,7 @@ from src import configuration as config
 
 
 class TestMarketPlace(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
+    def setUp(self):
         Clock.reset()
         config.num_spend_entries_per_day = 24
         config.num_win_entries_per_day = 24 * 60
@@ -37,6 +36,37 @@ class TestMarketPlace(unittest.TestCase):
             self.assertEqual(num_auctions_won_last_day,
                              num_auctions_per_iteration * config.num_iterations_per_day,
                              "expected the total number of wins in a day to be equal to the number of auctions")
+
+    def test_traffic_dist_mean_same_every_day(self):
+        n_days = 7
+        minutes_to_check = [0, config.num_iterations_per_day//4, config.num_iterations_per_day//2,
+                            config.num_iterations_per_day-1]
+        calculated_mean_per_minute = {
+            minute: [] for minute in minutes_to_check
+        }
+        for day in range(n_days):
+            for minute in minutes_to_check:
+                Clock._iterations = day * config.num_iterations_per_day + minute
+                calculated_mean = Marketplace._calculate_current_mean_num_of_auctions()
+                if day > 0:
+                    # Check that the calculated mean is equal to the mean calculated
+                    # in the same minute of the previous day
+                    self.assertEqual(calculated_mean, calculated_mean_per_minute[minute][-1])
+                calculated_mean_per_minute[minute].append(calculated_mean)
+
+    def test_traffic_dist_mean_single_cos_wave(self):
+        calculated_mean_histogram = {}
+        for _ in range(config.num_iterations_per_day):
+            calculated_mean = Marketplace._calculate_current_mean_num_of_auctions()
+            if calculated_mean_histogram.get(calculated_mean) is None:
+                calculated_mean_histogram[calculated_mean] = 1
+            else:
+                calculated_mean_histogram[calculated_mean] += 1
+            Clock.advance()
+        # check that each calculated value for the distribution mean appears at most twice in a given day
+        for calculated_mean, num_appearances in calculated_mean_histogram.items():
+            self.assertGreaterEqual(num_appearances, 1)
+            self.assertLessEqual(num_appearances, 2)
 
 
 if __name__ == '__main__':
