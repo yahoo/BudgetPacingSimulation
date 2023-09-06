4bf152ab8a8be442383d8ebc0736509c1c41d2f7
rabboud <rani.abboud@yahooinc.com>
2023-09-06 14:42:11 +0300

    nonlinear mystique: support for adding campaigns in the middle of the day

--- src/system/budget_pacing/mystique/mystique_tracked_campaign.py
+++ src/system/budget_pacing/mystique/mystique_tracked_campaign.py
@@ -40,6 +40,9 @@ class MystiqueTrackedCampaign:
         if len(self.today_ps) > 0:
             self.ps_history.append(self.today_ps)
         self.today_ps = []
+        if is_new_campaign and Clock.minute_in_day() > 0:
+            # if the campaign was added in the middle of the day, initialise today's ps history
+            self.today_ps += [mystique_constants.default_ps_value] * Clock.minute_in_day()
 
         # updating the spend history arr and initializing today's spend arr
         if len(self.today_spend) > 0:
